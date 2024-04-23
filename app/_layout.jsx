4766a442f09946db86b3bcd428c1ec6bc182b5a2
Santhosh <santhoshs19032003@gmail.com>
2024-04-23 16:16:15 +0530

    Icon Error Correction

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
 import { RecoilRoot } from "recoil";
+import { Feather } from "@expo/vector-icons";
 
 export default function _layout() {
   return (
